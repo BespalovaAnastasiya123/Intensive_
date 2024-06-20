29dd360bb0f8d85d462f940d144dc015684bd244
Anastasiya Bespalova <3537184@gmail.com>
2024-06-20 20:08:13 +0300

    внесены корректировки в код

---
+++ src/main/java/Lesson_14/entities/Factorial.java
@@ -0,0 +1,22 @@
+package Lesson_14.entities;
+import java.lang.IllegalArgumentException;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+
+    public static long factorial(int value) {
+        long result = 1;
+        if (value < 0) {
+            throw new IllegalArgumentException("Число дожно быть не отрицательным");
+        } else if (value == 0) {
+            return 1;
+        } else {
+            for (int i = 1; i <= value; i++) {
+                result *= i;
+            }
+        }
+        return result;
+    }
+}
