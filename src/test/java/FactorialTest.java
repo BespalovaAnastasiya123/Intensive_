265a1010ee41ee21821cab045697dcf21c45518b
Anastasiya Bespalova <3537184@gmail.com>
2024-06-29 22:27:12 +0300

    Исправлены замечания

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -4,14 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FactorialTest {
 
+
     @Test
     void testCalculateFactorial() {
-        assertEquals(1, Factorial.factorial(0));
-        assertEquals(1, Factorial.factorial(1));
-        assertEquals(2, Factorial.factorial(2));
-        assertEquals(6, Factorial.factorial(3));
-        assertEquals(24, Factorial.factorial(4));
-        assertEquals(120, Factorial.factorial(5));
+        assertAll("Вычисление факториала",
+                () -> assertEquals(1, Factorial.factorial(0)),
+                () -> assertEquals(1, Factorial.factorial(1)),
+                () -> assertEquals(2, Factorial.factorial(2)),
+                () -> assertEquals(6, Factorial.factorial(3)),
+                () -> assertEquals(24, Factorial.factorial(4)),
+                () -> assertEquals(120, Factorial.factorial(5))
+                );
     }
 
     @Test
